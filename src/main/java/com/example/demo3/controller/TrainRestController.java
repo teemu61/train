a66b3ada23f11e18264050a0bb43ac134dc854a3
Teemu Jappinen <teemu.jappinen@cgi.com>
2020-08-12 21:37:59 +0300

    add swagger docs.

--- src/main/java/com/example/demo3/controller/TrainRestController.java
+++ src/main/java/com/example/demo3/controller/TrainRestController.java
@@ -3,12 +3,12 @@ package com.example.demo3.controller;
 import com.example.demo3.pojo.Result;
 import com.example.demo3.pojo.Train;
 import com.example.demo3.service.TrainService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api")
 @RestController
 public class TrainRestController {
 
@@ -18,7 +18,9 @@ public class TrainRestController {
         this.trainService = trainService;
     }
 
-    @GetMapping(value = "/api/trains/{id}")
+    @ApiOperation(value = "GET location of train. {id} is the train number.",
+            notes="example: http://localhost:8080/api/trains/8448 ")
+    @GetMapping(value = "/trains/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Result getTrain(@PathVariable("id") Long id) throws Exception {
 
